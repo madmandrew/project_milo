c0db0de328141df5fe995397d694c6653cc0cea2
Andrew Barta <andrew@idahobartas.com>
2016-12-07 17:34:26 -0700

    update predictions

--- main.py
+++ main.py
@@ -1,161 +1,228 @@
 from sklearn.neural_network import MLPClassifier
 from pymongo import MongoClient
+import random
 
-def get_team_name_lookup():
-    teamNameMap = {"N Colorado": "Northern Colorado", "Boston Univ": "Boston College", "VA Commonwealth": "VCU",
-                   "St Peter's": "Saint Peter's", "St John's": "St. John's", "BYU": "Brigham Young",
-                   "Santa Barbara": "UC Santa Barbara", "WKU": "Western Kentucky", "S Dakota St": "South Dakota",
-                   "St Louis": "Saint Louis", "Southern Miss": "Southern Mississippi",
-                   "St Bonaventure": "St. Bonaventure", "Loyola MD": "Loyola Marymount", "NC State": "North Carolina",
-                   "St Mary's CA": "Saint Mary's", "NC A&T": "North Carolina A&T", "Albany NY": "Albany",
-                   "Miami FL": "Miami (FL)", "Southern Univ": "Southern", "Mississippi": "Mississippi State",
-                   "Northwestern LA": "Northwestern State", "FL Gulf Coast": "Florida Gulf Coast",
-                   "SF Austin": "Stephen F. Austin", "W Michigan": "Western Michigan", "E Kentucky": "Eastern Kentucky",
-                   "Coastal Car": "Coastal Carolina", "G Washington": "George Washington",
-                   "NC Central": "North Carolina Central", "St Joseph's PA": "Saint Joseph's",
-                   "WI Milwaukee": "Milwaukee", "N Dakota St": "North Dakota State", "ULL": "Louisiana-Lafayette ",
-                   "American Univ": "American University", "Cal Poly SLO": "Cal Poly", "TX Southern": "Texas Southern",
-                   "E Washington": "Eastern Washington", "SMU": "Southern Methodist", "WI Green Bay": "Green Bay",
-                   "CS Bakersfield": "Cal State Bakersfield", "Ark Little Rock": "Little Rock",
-                   "MTSU": "Middle Tennessee State", "UT San Antonio": "Texas-San Antonio",
-                   "Indiana St": "Indiana State", "San Diego St": "San Diego State", "Penn St": "Penn State",
-                   "Morehead St": "Morehead State", "Florida St": "Florida State", "Kansas St": "Kansas State",
-                   "Utah St": "Utah State", "Michigan St": "Michigan State", "Iowa St": "Iowa State",
-                   "Wichita St": "Wichita State", "New Mexico St": "New Mexico State",
-                   "Long Beach St": "Long Beach State", "Murray St": "Murray State", "Colorado St": "Colorado State",
-                   "Norfolk St": "Norfolk State", "Ohio St": "Ohio State", "Oklahoma St": "Oklahoma State",
-                   "Weber St": "Weber State", "Arizona St": "Arizona State", "Georgia St": "Georgia State",
-                   "Oregon St": "Oregon State", "Fresno St": "Fresno State"}
-    return teamNameMap
+class MadnessPredictor(object):
+    def __init__(self, layers):
+        # Create the neural network
+        # http://scikit-learn.org/stable/modules/generated/sklearn.neural_network.MLPClassifier.html#sklearn.neural_network.MLPClassifier
+        self.neural_network = MLPClassifier(hidden_layer_sizes=(layers), activation='logistic', solver='lbfgs', alpha=1e-05,
+                                       learning_rate_init=0.001,
+                                       random_state=1)
+        self.team_name_lookup = self.get_team_name_lookup()
+        self.train_neural_network()
 
-def get_training_data(year, isTestData):
-    """Pull data from the db and creates training features based on match ups"""
-    teamNameLookup = get_team_name_lookup()
+    def get_team_name_lookup(self):
+        teamNameMap = {"N Colorado": "Northern Colorado", "Boston Univ": "Boston College", "VA Commonwealth": "VCU",
+                       "St Peter's": "Saint Peter's", "St John's": "St. John's", "BYU": "Brigham Young",
+                       "Santa Barbara": "UC Santa Barbara", "WKU": "Western Kentucky", "S Dakota St": "South Dakota",
+                       "St Louis": "Saint Louis", "Southern Miss": "Southern Mississippi",
+                       "St Bonaventure": "St. Bonaventure", "Loyola MD": "Loyola Marymount",
+                       "NC State": "North Carolina",
+                       "St Mary's CA": "Saint Mary's", "NC A&T": "North Carolina A&T", "Albany NY": "Albany",
+                       "Miami FL": "Miami (FL)", "Southern Univ": "Southern", "Mississippi": "Mississippi State",
+                       "Northwestern LA": "Northwestern State", "FL Gulf Coast": "Florida Gulf Coast",
+                       "SF Austin": "Stephen F. Austin", "W Michigan": "Western Michigan",
+                       "E Kentucky": "Eastern Kentucky",
+                       "Coastal Car": "Coastal Carolina", "G Washington": "George Washington",
+                       "NC Central": "North Carolina Central", "St Joseph's PA": "Saint Joseph's",
+                       "WI Milwaukee": "Milwaukee", "N Dakota St": "North Dakota State", "ULL": "Louisiana-Lafayette ",
+                       "American Univ": "American University", "Cal Poly SLO": "Cal Poly",
+                       "TX Southern": "Texas Southern",
+                       "E Washington": "Eastern Washington", "SMU": "Southern Methodist", "WI Green Bay": "Green Bay",
+                       "CS Bakersfield": "Cal State Bakersfield", "Ark Little Rock": "Little Rock",
+                       "MTSU": "Middle Tennessee State", "UT San Antonio": "Texas-San Antonio",
+                       "Indiana St": "Indiana State", "San Diego St": "San Diego State", "Penn St": "Penn State",
+                       "Morehead St": "Morehead State", "Florida St": "Florida State", "Kansas St": "Kansas State",
+                       "Utah St": "Utah State", "Michigan St": "Michigan State", "Iowa St": "Iowa State",
+                       "Wichita St": "Wichita State", "New Mexico St": "New Mexico State",
+                       "Long Beach St": "Long Beach State", "Murray St": "Murray State",
+                       "Colorado St": "Colorado State",
+                       "Norfolk St": "Norfolk State", "Ohio St": "Ohio State", "Oklahoma St": "Oklahoma State",
+                       "Weber St": "Weber State", "Arizona St": "Arizona State", "Georgia St": "Georgia State",
+                       "Oregon St": "Oregon State", "Fresno St": "Fresno State"}
+        return teamNameMap
 
-    # Connect to db
-    client = MongoClient('localhost:27017')
-    db = client.march_madness
+    def get_training_data(self, year, isTestData):
+        """Pull data from the db and creates training features based on match ups"""
 
-    training_features = []
-    training_targets = []
+        # Connect to db
+        client = MongoClient('localhost:27017')
+        db = client.march_madness
 
-    # For each match in march madness of the given year (find all the teams that played in the tournament that year)
-    for match in db.tournament_stats.find({'Bracket Year': year}):
-        # if its training data we only grab first round matches
-        if (not isTestData or int(match['Match Number']) < 33):
+        training_features = []
+        training_targets = []
 
-            team_name_one = teamNameLookup[match['Team 1']] if match['Team 1'] in teamNameLookup else match['Team 1']
-            team_name_two = teamNameLookup[match['Team 2']] if match['Team 2'] in teamNameLookup else match['Team 2']
+        # For each match in march madness of the given year (find all the teams that played in the tournament that year)
+        for match in db.tournament_stats.find({'Bracket Year': year}):
+            # if its training data we only grab first round matches
+            if (not isTestData or int(match['Match Number']) < 33):
 
-            # Grab the regular season stats of the two teams that played each other
-            team_one = db.reg_season_stats.find_one({'Year': year, "Team": team_name_one})
-            team_two = db.reg_season_stats.find_one({'Year': year, "Team": team_name_two})
+                team_name_one = self.team_name_lookup[match['Team 1']] if match['Team 1'] in self.team_name_lookup else match['Team 1']
+                team_name_two = self.team_name_lookup[match['Team 2']] if match['Team 2'] in self.team_name_lookup else match['Team 2']
 
-            if team_one is None:
-                print(match['Team 1'])
-            if team_two is None:
-                print(match['Team 2'])
+                # Grab the regular season stats of the two teams that played each other
+                team_one = db.reg_season_stats.find_one({'Year': year, "Team": team_name_one})
+                team_two = db.reg_season_stats.find_one({'Year': year, "Team": team_name_two})
 
-            # Make sure they were actually playing each other that round
-            if (team_one is not None and team_two is not None):
-                # Add them to the data (the two team's stats and who won
-                match_up = (team_one, team_two)
-                training_features.append(match_up)
-                training_targets.append(match['Winner'])
+                if team_one is None:
+                    print(match['Team 1'])
+                if team_two is None:
+                    print(match['Team 2'])
 
-    return training_features, training_targets
+                # Make sure they were actually playing each other that round
+                if (team_one is not None and team_two is not None):
+                    # Add them to the data (the two team's stats and who won
+                    match_up = (team_one, team_two)
+                    training_features.append(match_up)
+                    training_targets.append(match['Winner'])
 
-def convert_and_order_data(feature, target):
-    """
-    This method orders the teams data to make sure inputs are consistent (since documents/dictionaries are unordered
+        return training_features, training_targets
 
-    Also change winning team to a 0 or 1
-    """
-    input = []
+    def convert_and_order_data(self, feature, target):
+        """
+        This method orders the teams data to make sure inputs are consistent (since documents/dictionaries are unordered
 
-    if(target == feature[0]["Team"]):
-        target = 0
-    else:
-        target = 1
+        Also change winning team to a 0 or 1
+        """
+        input = []
 
-    input.append(float(feature[0]['PPG']))
-    input.append(float(feature[0]['GP']))
-    input.append(float(feature[0]['MPG']))
-    input.append(float(feature[0]['FGM']))
-    input.append(float(feature[0]['FGA']))
-    input.append(float(feature[0]['FG%']))
-    input.append(float(feature[0]['3PM']))
-    input.append(float(feature[0]['3PA']))
-    input.append(float(feature[0]['3P%']))
-    input.append(float(feature[0]['FTM']))
-    input.append(float(feature[0]['FTA']))
-    input.append(float(feature[0]['FT%']))
-    input.append(float(feature[0]['TOV']))
-    input.append(float(feature[0]['PF']))
-    input.append(float(feature[0]['ORB']))
-    input.append(float(feature[0]['DRB']))
-    input.append(float(feature[0]['RPG']))
-    input.append(float(feature[0]['APG']))
-    input.append(float(feature[0]['SPG']))
-    input.append(float(feature[0]['BPG']))
+        if(target == feature[0]["Team"]):
+            target = 0
+        else:
+            target = 1
 
-    input.append(float(feature[1]['PPG']))
-    input.append(float(feature[1]['GP']))
-    input.append(float(feature[1]['MPG']))
-    input.append(float(feature[1]['FGM']))
-    input.append(float(feature[1]['FGA']))
-    input.append(float(feature[1]['FG%']))
-    input.append(float(feature[1]['3PM']))
-    input.append(float(feature[1]['3PA']))
-    input.append(float(feature[1]['3P%']))
-    input.append(float(feature[1]['FTM']))
-    input.append(float(feature[1]['FTA']))
-    input.append(float(feature[1]['FT%']))
-    input.append(float(feature[1]['TOV']))
-    input.append(float(feature[1]['PF']))
-    input.append(float(feature[1]['ORB']))
-    input.append(float(feature[1]['DRB']))
-    input.append(float(feature[1]['RPG']))
-    input.append(float(feature[1]['APG']))
-    input.append(float(feature[1]['SPG']))
-    input.append(float(feature[1]['BPG']))
+        input.append(float(feature[0]['PPG']))
+        input.append(float(feature[0]['GP']))
+        input.append(float(feature[0]['MPG']))
+        input.append(float(feature[0]['FGM']))
+        input.append(float(feature[0]['FGA']))
+        input.append(float(feature[0]['FG%']))
+        input.append(float(feature[0]['3PM']))
+        input.append(float(feature[0]['3PA']))
+        input.append(float(feature[0]['3P%']))
+        input.append(float(feature[0]['FTM']))
+        input.append(float(feature[0]['FTA']))
+        input.append(float(feature[0]['FT%']))
+        input.append(float(feature[0]['TOV']))
+        input.append(float(feature[0]['PF']))
+        input.append(float(feature[0]['ORB']))
+        input.append(float(feature[0]['DRB']))
+        input.append(float(feature[0]['RPG']))
+        input.append(float(feature[0]['APG']))
+        input.append(float(feature[0]['SPG']))
+        input.append(float(feature[0]['BPG']))
 
-    return input, target
+        input.append(float(feature[1]['PPG']))
+        input.append(float(feature[1]['GP']))
+        input.append(float(feature[1]['MPG']))
+        input.append(float(feature[1]['FGM']))
+        input.append(float(feature[1]['FGA']))
+        input.append(float(feature[1]['FG%']))
+        input.append(float(feature[1]['3PM']))
+        input.append(float(feature[1]['3PA']))
+        input.append(float(feature[1]['3P%']))
+        input.append(float(feature[1]['FTM']))
+        input.append(float(feature[1]['FTA']))
+        input.append(float(feature[1]['FT%']))
+        input.append(float(feature[1]['TOV']))
+        input.append(float(feature[1]['PF']))
+        input.append(float(feature[1]['ORB']))
+        input.append(float(feature[1]['DRB']))
+        input.append(float(feature[1]['RPG']))
+        input.append(float(feature[1]['APG']))
+        input.append(float(feature[1]['SPG']))
+        input.append(float(feature[1]['BPG']))
 
-def train_neural_network():
+        return input, target
 
-    training_years = ['2011', '2012', '2013', '2014', '2015']
+    def train_neural_network(self):
 
-    # Create the neural network
-    # http://scikit-learn.org/stable/modules/generated/sklearn.neural_network.MLPClassifier.html#sklearn.neural_network.MLPClassifier
-    neural_network = MLPClassifier(hidden_layer_sizes=(10, 2), activation='logistic', solver='lbfgs', alpha=1e-05,
-                                   learning_rate_init=0.001,
-                                   random_state=1)
+        training_years = ['2011', '2012', '2013', '2014', '2015']
 
-    for year in training_years:
-        training_features, training_targets = get_training_data(year, isTestData=False)
+        for year in training_years:
+            training_features, training_targets = self.get_training_data(year, isTestData=False)
+
+            nn_inputs = []
+            nn_targets = []
+
+            # For each match-up
+            for index, match_up in enumerate(training_features):
+                # Format the data for the Neural Network
+                feature, target = self.convert_and_order_data(match_up, training_targets[index])
+                # Set it to send to the neural network
+                nn_inputs.append(feature)
+                nn_targets.append(target)
+
+            # Train the neural Network
+            self.neural_network.fit(nn_inputs, nn_targets)
+
+    def predict_year(self, year, print_team_names):
+        training_features, training_targets = self.get_training_data(year, isTestData=True)
+        validation_features, validation_targets = self.get_training_data(year, isTestData=False)
 
         nn_inputs = []
-        nn_targets = []
-
-        # For each match-up
         for index, match_up in enumerate(training_features):
-            # Format the data for the Neural Network
-            feature, target = convert_and_order_data(match_up, training_targets[index])
+            feature, target = self.convert_and_order_data(match_up, training_targets[index])
             # Set it to send to the neural network
             nn_inputs.append(feature)
-            nn_targets.append(target)
+            #winnerIndex = self.neural_network.predict(feature)[0]
+            #winnerName = match_up[winnerIndex]['Team']
+            #print("Predicted: {} | Actual: {}".format(winnerName, training_targets[index]))
 
-        # Train the neural Network
-        neural_network.fit(nn_inputs, nn_targets)
 
-def predict():
-    training_features, training_targets = get_training_data('2016', isTestData=True)
+        results = self.neural_network.predict(nn_inputs)
+        correct = 0
+        total = len(results)
+        for index, result in enumerate(results):
+            predictedWinnerName = training_features[index][result]['Team']
+            actualWinnerName = training_targets[index]
+            if actualWinnerName in self.team_name_lookup:
+                actualWinnerName = self.team_name_lookup[actualWinnerName]
 
-    print("TEST")
+            if (print_team_names):
+                pass
+                # print("Predicted: {} | Actual: {}".format(predictedWinnerName, actualWinnerName))
+            if predictedWinnerName == actualWinnerName:
+                correct += 1
+            else:
+                print("wrong... Predicted: {} | Actual: {}".format(predictedWinnerName, actualWinnerName))
 
+        print("{} / {} correct {}%".format(correct, total, (correct / total) * 100))
+        return (correct / total)
+
+
+def programmatic_layer_checker():
+    accuracy = 0
+    bestLayers = None
+    # for i in range(2, 8):
+    #     for j in range(2, 21):
+    #         layers = [j]*i
+    #         layers.append(2)
+    #         madnessPredictor = MadnessPredictor(layers)
+    #         tempAccuracy = madnessPredictor.predict_year('2016')
+    #         print("{} accuracy = {}%".format(layers, (tempAccuracy * 100)))
+    #         if (tempAccuracy > accuracy):
+    #             accuracy = tempAccuracy
+    #             bestLayers = layers
+
+    for iteration in range(1, 50):
+        for i in range(3, 8):
+            layers = []
+            for j in range(1, i):
+                layers.append(random.randint(1, 20))
+            layers.append(2)
+            madnessPredictor = MadnessPredictor(layers)
+            tempAccuracy = madnessPredictor.predict_year('2016', print_team_names=False)
+            print("{} accuracy = {}%".format(layers, (tempAccuracy * 100)))
+            if (tempAccuracy > accuracy):
+                accuracy = tempAccuracy
+                bestLayers = layers
+
+    print("Best accuracy: {} | Layers = {}".format(accuracy, bestLayers))
 
 if __name__ == "__main__":
     print("Starting Neural Network")
-    train_neural_network()
-    predict()
+    madnessPredictor = MadnessPredictor([16, 16, 16, 2])
+    madnessPredictor.predict_year('2016', print_team_names=True)
+    #programmatic_layer_checker()
